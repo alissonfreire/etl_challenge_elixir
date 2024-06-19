5676df5749b72bd0db9779847aad653d10e0499f
Alisson Freire <alissonfreire2@gmail.com>
2024-06-18 22:25:43 -0300

    fix: adjust attempt colunm

--- priv/repo/migrations/20240618001454_add_info_table.exs
+++ priv/repo/migrations/20240618001454_add_info_table.exs
@@ -3,10 +3,10 @@ defmodule EtlChallenge.Repo.Migrations.AddInfoTable do
 
   def change do
     create table(:info) do
-      add :attempt, :string
+      add :attempt, :integer, default: 0
       add :is_finished, :boolean, default: false
-      add :success_pages, :integer, default: 0
       add :fetched_pages, :integer, default: 0
+      add :success_pages, :integer, default: 0
       add :failed_pages, :integer, default: 0
       add :last_page, :integer, default: 0
       add :last_stoped_page, :integer, default: 0
