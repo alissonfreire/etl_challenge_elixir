7ece51c5e61c5753ec007788698f3e3280fb4495
Alisson Freire <alissonfreire2@gmail.com>
2024-06-18 21:09:42 -0300

    feat: added database config

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :etl_challenge, EtlChallenge.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "etl_challenge_dev",
+  hostname: System.get_env("DB_HOST", "localhost"),
+  username: System.get_env("DB_USER", "postgres"),
+  password: System.get_env("DB_PASS", "postgres"),
+  database: System.get_env("DB_DATABASE", "etl_challenge_dev"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
